a6893c3bc7c5d3fc0441a3564dc03421233895f8
ziggyC <alexrafati@gmail.com>
2016-06-14 12:57:16 -0700

    Add files via upload

---
+++ GoogleAPI/Request_Model/AddressAddRequestModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sabio.Web.Models.Requests
+{
+    public class AddressAddRequestModel
+
+    {
+
+
+        [Required]
+        public string Line { get; set; }
+
+
+        public string LineTwo { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string StateId { get; set; }
+
+        [Required]
+        public int ZipCode { get; set; }
+
+
+        public string UserID { get; set; }
+
+
+        public decimal? Latitude { get; set; }
+
+
+        public decimal? Longitude { get; set; }
+
+    }
+}
