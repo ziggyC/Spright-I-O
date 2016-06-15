23869305043f263ba736e6ffe9be4a05346d42ae
Alex Rafati <alexrafati@gmail.com>
2016-06-15 12:53:30 -0700

    adding addressservice factory

---
+++ GoogleAPI/Applications/ServiceFactory/addressServices.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME)
+        .factory('$AddressesService', addressesServiceFactory);
+
+    addressesServiceFactory.$inject = ['$baseService', '$sabio'];
+
+    function addressesServiceFactory($baseService, $sabio) {
+
+        var aSabioServiceObject = sabio.services.addresses;
+        var newService = $baseService.merge(true, {}, aSabioServiceObject, $baseService);
+
+        console.log('addresses list:', aSabioServiceObject)
+        return newService;
+    }
+})();
+
